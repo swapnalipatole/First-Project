e637ee82de6a49519f5deeee140e05748be344c2
swapnalipatole <swapnalipatole28@gmail.com>
2023-07-31 14:02:16 +0530

    First Comit

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+
+    public double Add(double a, double b){
+        return a+b;
+    }
+    public double Sub(double a, double b){
+        return a-b;
+    }
+    public double Multi(double a, double b){
+        return a*b;
+    }
+    public double Div(double a, double b){
+        return a/b;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+
+        double x = cal.Add(2.5,3.4);
+        System.out.println("Addition = " + x);
+
+        x = cal.Sub(8.5,2);
+        System.out.println("Subtration = " + x);
+
+        x = cal.Multi(3,6);
+        System.out.println("Multiplication = " + x);
+
+        x = cal.Div(20, 2);
+        System.out.println("Division = " + x);
+    }
+}
